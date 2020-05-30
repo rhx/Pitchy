203a9e99cc8bf6a044ffb3f145cd8c692889684b
Rene Hexel <r.hexel@griffith.edu.au>
2020-05-30 20:06:45 +1000

    Update to Swift 5.2

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
         .library(name: "Pitchy", targets: ["Pitchy"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", .branch("master")),
+        .package(url: "https://github.com/Quick/Quick.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
